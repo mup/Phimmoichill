8ebf486ca2248b949101c8be11707b1bc5ac8c46
kaorkidev <prengki.eka@gmail.com>
2024-11-22 21:08:07 +0800

    Update KuramanimeProvider.kt
    change domain

--- KuramanimeProvider/src/main/kotlin/com/hexated/KuramanimeProvider.kt
+++ KuramanimeProvider/src/main/kotlin/com/hexated/KuramanimeProvider.kt
@@ -8,7 +8,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class KuramanimeProvider : MainAPI() {
-    override var mainUrl = "https://kuramanime.pro"
+    override var mainUrl = "https://kuramanime.red"
     override var name = "Kuramanime"
     override val hasQuickSearch = false
     override val hasMainPage = true
